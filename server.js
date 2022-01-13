f85b2fea5f78352cab6ea429be49342ed485a3d0
kongk1234 <kasper.lindebarrith@gmail.com>
2022-01-13 11:09:26 +0100

    Working on files

--- server.js
+++ server.js
@@ -63,8 +63,8 @@ app.post("/files", function(req, res) {
 				});
 			}
 		});
+		res.send({Message: "Success", Status: 200});
 	});
-	res.send({Message: "Success", Status: 200});
 });
 
 app.listen(42069)
